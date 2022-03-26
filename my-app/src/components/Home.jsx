8b97abc5a6095286bff848811b26f45a8cb949db
pabina <rimalpabina1234@gmail.com>
2022-03-26 21:38:43 +0545

    page update

---
+++ my-app/src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from './Navigation';
+import Slider from './Slider';
+import Brands from './Brands';
+import Carasole from './Carasole';
+
+const Home = () => {
+  return (
+    <div>
+      <Navigation/>
+      <Slider/>
+     <Brands/>
+     <Carasole/>
+
+    </div>
+  )
+}
+
+export default Home
---
+++ my-app/src/pages/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Home from '../components/Home';
+
+const Main = () => {
+  return (
+    <div>
+      <Header/>
+      <Home/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Main
